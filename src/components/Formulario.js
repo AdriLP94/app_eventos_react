e9f4e2b6bf6ef37fa36d3c3b6cc76a99bd327236
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-09-24 20:37:41 +0200

    Update Formulario.js

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import {CategoriasConsumer} from "../context/CategoriasContext";
+import {EventosConsumer} from "../context/EventosContext";
 
 class Formulario extends Component {
 
     state = {
         nombre : "",
-        categoria : ""
+        categoria : "",
+        ordenar : "date"
     }
 
     obtenerDatosEvento = e => {
@@ -17,53 +19,75 @@ class Formulario extends Component {
 
     render() {
         return (
-            <form>
-                <fieldset className="uk-fieldset uk-margin">
-                    <legend className="uk-legend uk-text-center">
-                        Busca tu evento por nombre o categoría
-                    </legend>
-                </fieldset>
+            <EventosConsumer>
+                {(value) => {
+                    return (
+                    <form
+                        onSubmit = {(e) => {
+                            e.preventDefault();
+                            value.obtenerEventos(this.state);
+                        }}
+                    >
+                        <fieldset className="uk-fieldset uk-margin">
+                            <legend className="uk-legend uk-text-center">
+                                Busca tu evento por nombre o categoría
+                            </legend>
+                        </fieldset>
 
-                <div className="uk-column-1-3@m uk-margin">
-                    <div className="uk-margin" uk-margin="true">
-                        <input 
-                            name="nombre"
-                            className="uk-input"
-                            type="text"
-                            placeholder="Nombre de evento o ciudad"
-                            onChange={this.obtenerDatosEvento}
-                        />
-                    </div>
+                        <div className="uk-column-1-4@m uk-margin">
+                            <div className="uk-margin" uk-margin="true">
+                                <input 
+                                    name="nombre"
+                                    className="uk-input"
+                                    type="text"
+                                    placeholder="Nombre de evento o ciudad"
+                                    onChange={this.obtenerDatosEvento}
+                                />
+                            </div>
 
-                    <div className="uk-margin" uk-margin="true">
-                        <select
-                            name="categoria"
-                            className="uk-select"
-                            onChange={this.obtenerDatosEvento}
-                        >
-                            <option value="">---Selecciona categoria---</option>
-                            <CategoriasConsumer>
-                                {(value) =>{
-                                    return (value.categorias.map(categoria =>(
-                                        <option key={categoria.id} value={categoria.id} data-uk-form-select>
-                                            {categoria.name_localized}
-                                        </option>
-                                    )))
-                                }}
-                            </CategoriasConsumer>
-                        </select>
-                    </div>
+                            <div className="uk-margin" uk-margin="true">
+                                <select
+                                    name="categoria"
+                                    className="uk-select"
+                                    onChange={this.obtenerDatosEvento}
+                                >
+                                    <option value="">---Selecciona categoria---</option>
+                                    <CategoriasConsumer>
+                                        {(value) =>{
+                                            return (value.categorias.map(categoria =>(
+                                                <option key={categoria.id} value={categoria.id} data-uk-form-select>
+                                                    {categoria.name_localized}
+                                                </option>
+                                            )))
+                                        }}
+                                    </CategoriasConsumer>
+                                </select>
+                            </div>
 
-                    <div className="uk-margin" uk-margin="true">
-                        <input 
-                            className="uk-button uk-button-danger"
-                            type="submit"
-                            value="Buscar"
-                        />
-                    </div>
+                            <div className="uk-margin" uk-margin="true">
+                                <select
+                                    name="ordenar"
+                                    className="uk-select"
+                                    onChange={this.obtenerDatosEvento}
+                                >
+                                    <option value="date">ordenar por fecha</option>
+                                    <option value="best">ordenar por valoracion</option>
+                                </select>
+                            </div>
+                            <div className="uk-margin" uk-margin="true">
+                                <input 
+                                    className="uk-button uk-button-danger"
+                                    type="submit"
+                                    value="Buscar"
+                                />
+                            </div>
 
-                </div>
-            </form>
+                        </div>
+                    </form>
+                )
+                }}
+                
+            </EventosConsumer>
         );
     }
 }
